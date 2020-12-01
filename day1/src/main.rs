2f14dba4e9d45637cb157da244b607e646843862
mattlennon3 <mattlennon3@hotmail.co.uk>
2020-12-01 18:06:05 +0000

    Day 1 - Part 2 - Done

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,26 +1,27 @@
-use std::io::prelude::*;
-use std::io;
 use std::fs;
+use std::io;
+use std::io::prelude::*;
 
 fn main() {
-    
     let file = fs::File::open("input.txt").expect("No input found");
     let file = io::BufReader::new(file);
-    
-    let numbers: Vec<u32> = file.lines()
+
+    let numbers: Vec<u32> = file
+        .lines()
         .map(|line| line.expect("error reading line"))
         .map(|line| line.parse::<u32>().expect("could not parse to u32"))
         .collect();
-    
+
     let target = 2020;
 
     for a in &numbers {
         for b in &numbers {
-            if a == b { continue };
-            if (a + b) == target {
-                println!("Found 2020! A: {}, B: {}", a, b);
-                println!("Multiplied: {}", a * b);
-                return ();
+            for c in &numbers {
+                if (a + b + c) == target {
+                    println!("Found 2020! A: {}, B: {}, C: {}", a, b, c);
+                    println!("Multiplied: {}", a * b * c);
+                    return ();
+                }
             }
         }
     }
